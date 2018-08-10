4e07a6803f98472b2bf964b5e41ca44cddbea677
mikeias07 <mikeias07@hotmail.com>
2018-08-09 23:24:36 -0300

    config linux

--- projeto_GESC/config/database.php
+++ projeto_GESC/config/database.php
@@ -42,10 +42,10 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3366'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'gesc'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'KWWM1k3ias'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

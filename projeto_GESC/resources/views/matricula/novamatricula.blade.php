ae937530f0348a57f17170d73baa581b5b96949b
Carlos Alexandre <vitorino.17alexandre@gmail.com>
2018-08-11 01:31:45 -0300

    Inicio da atualização de layout

--- projeto_GESC/resources/views/matricula/novamatricula.blade.php
+++ projeto_GESC/resources/views/matricula/novamatricula.blade.php
@@ -22,8 +22,8 @@
     <div class="tab-pane fade show active" id="identificacao" role="tabpanel" aria-labelledby="identificacao-tab">
     
         
-    <div  class="d-flex p-2">
-
+    <div >
+    </br>
 
     <form  action="/novaMatricula/adiciona" method="POST" 
     onsubmit="return validarMatricula(matriculaNova.nomecrianca, matriculaNova.datanascimentocrianca, matriculaNova.rgcrianca,
@@ -33,113 +33,133 @@
     matriculaNova.salarioresp2, matriculaNova.tel1resp2, matriculaNova.tel2resp2, matriculaNova.numnis);" name="matriculaNova">
             <div class="form-group ">
             {{ csrf_field() }}
-            <div class="form-group" >
-                <label>Nome</label>
-                <input type="text" class="form-control" name="nomecrianca" maxlength="255" autocomplete="off">
-                <spam id="msgNomeCrianca"></spam>
-            </div>
+            <div class="form-group ">
+                <div class="row">
+                    <div class="col-sm-8" >
+                        <label>Nome</label>
+                        <input type="text" class="form-control" name="nomecrianca" maxlength="255" autocomplete="off">
+                        <spam id="msgNomeCrianca"></spam>
+                    </div>
+                        
+                    <div class="col-sm-2">
+                        <label>Nascimento</label>
+                        <input type="date" class="form-control" name="datanascimentocrianca">
+                        <spam id="msgDataNascimento"></spam>
+                    </div>
 
-                
-            <div class="form-group">
-                <label>Nascimento</label>
-                <input type="date" class="form-control" name="datanascimentocrianca">
-                <spam id="msgDataNascimento"></spam>
+                    <div class="col-sm-2">
+                        <label>Sexo</label>
+                        <select name="sexocrianca" id="" class="custom-select" value="">
+                            <option value="M">Masculino</option>
+                            <option value="F">Feminino</option>
+                        </select>
+                    </div>
+                </div>
             </div>
+            
+            <div class="form-group ">
+                <div class="row" >
+                    <div class="col-sm-4">
+                        <label>RG</label>
+                        <input type="text" class="form-control" id="rgcrianca" name="rgcrianca" onkeyup="mascara(this, Rg);" maxlength="9" autocomplete="off">
+                        <spam id="msgRg"></spam>
+                    </div>
 
-            <div class="form-group">
-                <label>Sexo</label>
-                <select name="sexocrianca" id="" class="custom-select" value="">
-                    <option value="M">Masculino</option>
-                    <option value="F">Feminino</option>
-                </select>
-            </div>
+                    <div class="col-sm-4">
+                        <label>CPF</label>
+                        <input type="text" class="form-control" name="cpfcrianca" id="cpfcrianca" maxlength="11" autocomplete="off" onkeyup="mascara(this, Cpf);">
+                        <spam id="msgCpf"></spam>
+                    </div>
 
-            <div class="form-group">
-                <label>RG</label>
-                <input type="text" class="form-control" id="rgcrianca" name="rgcrianca" onkeyup="mascara(this, Rg);" maxlength="9" autocomplete="off">
-                <spam id="msgRg"></spam>
-            </div>
-
-            <div class="form-group">
-                <label>CPF</label>
-                <input type="text" class="form-control" name="cpfcrianca" id="cpfcrianca" maxlength="11" autocomplete="off" onkeyup="mascara(this, Cpf);">
-                <spam id="msgCpf"></spam>
-            </div>
-
-            <div class="form-group">
-                <label>CEP</label>
-                <input type="text" class="form-control" name="cep" id="cep" maxlength="8" autocomplete="off" onkeyup="mascara(this, Cep);">
-                <spam id="msgCep"></spam>
-            </div>
-
-            <div class="form-group">
-                <label>Endereço</label>
-                <input type="text" class="form-control" name="logradouro" maxlength="255" autocomplete="off">
-                <spam id="msgEndereco"></spam>
-            </div>
-
-            <div class="form-group">
-                <label>Bairro</label>
-                <input type="text" class="form-control" name="bairro" maxlength="255" autocomplete="off">
-                <spam id="msgBairro"></spam>
+                    <div class="col-sm-4">
+                        <label>CEP</label>
+                        <input type="text" class="form-control" name="cep" id="cep" maxlength="8" autocomplete="off" onkeyup="mascara(this, Cep);">
+                        <spam id="msgCep"></spam>
+                    </div>
+                </div>
             </div>
 
             <div class="form-group ">
-                <label>Complemento</label>
-                <input type="text" class="form-control" name="complemento" maxlength="255" autocomplete="off">
+                <div class="row" >
+                    <div class="col-sm-8">
+                        <label>Endereço</label>
+                        <input type="text" class="form-control" name="logradouro" maxlength="255" autocomplete="off">
+                        <spam id="msgEndereco"></spam>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <label>Bairro</label>
+                        <input type="text" class="form-control" name="bairro" maxlength="255" autocomplete="off">
+                        <spam id="msgBairro"></spam>
+                    </div>
+                </div>
             </div>
 
-            <div class="form-group">
-                <label>CRAS/CREAS</label>
-                <select name="cras" id="" class="custom-select" name="cras">
-                    @foreach($cras as $c)
-                        <option value="{{ $c->id}}">{{ $c->nomeCras }}</option>
-                    @endforeach
-                </select>
+            <div class="form-group ">
+                <div class="row" >
+                    <div class="col-sm-6">
+                        <label>Complemento</label>
+                        <input type="text" class="form-control" name="complemento" maxlength="255" autocomplete="off">
+                    </div>
+
+                    <div class="col-sm-6">
+                        <label>CRAS/CREAS</label>
+                        <select name="cras" id="" class="custom-select" name="cras">
+                            @foreach($cras as $c)
+                                <option value="{{ $c->id}}">{{ $c->nomeCras }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
             </div>
 
-            <div class="form-group">
-                <label>publico priritario</label>
-                <select name="pprioritario" id="" class="custom-select">
-                    @foreach($pprioritario as $p)
-                        <option value="{{ $p->idpublicoprioritario }}">{{ $p->publicoprioritario }}</option>
-                    @endforeach
-
-                    
-                </select>
+            <div class="form-group " >
+                <div class="row" >
+                    <div class="col-sm-6">
+                        <label>Público priritario</label>
+                        <select name="pprioritario" id="" class="custom-select">
+                            @foreach($pprioritario as $p)
+                                <option value="{{ $p->idpublicoprioritario }}">{{ $p->publicoprioritario }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
             </div>
-
             
 
             <div class="form-group">
-                <label>Escola</label>
-                <select name="escola" id="" class="custom-select" >
-                    @foreach($escola as $e)
-                        <option value="{{ $e->idescola }}">{{ $e->nomeescola }}</option>
-                    @endforeach
-                </select>
-
+                <div class="row">
+                    <div class="col-sm-6">
+                        <label>Escola</label>
+                        <select name="escola" id="" class="custom-select" >
+                            @foreach($escola as $e)
+                                <option value="{{ $e->idescola }}">{{ $e->nomeescola }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
             </div>
 
             <div class="form-group">
-                <label>Serie Escolar</label>
-                <select name="serie" id="" class="custom-select">
-                    
-                    <option value="1">1º Fundamental</option>
-                    <option value="2">2º Fundamental</option>
-                    <option value="3">3º Fundamental</option>
-                    <option value="4">4º Fundamental</option>
-                    <option value="5">5º Fundamental</option>
-                    <option value="6">6º Fundamental</option>
-                    <option value="7">7º Fundamental</option>
-                    <option value="8">8º Fundamental</option>
-                    <option value="9">9º Fundamental</option>
-                    <option value="10">1º Médio</option>
-                    <option value="11">2º Médio</option>
-                    <option value="12">3º Médio</option>
-                        
-                </select>
-
+                <div class="row">
+                    <div class="col-sm-3">
+                        <label>Serie Escolar</label>
+                        <select name="serie" id="" class="custom-select"> 
+                            <option value="1">1º Fundamental</option>
+                            <option value="2">2º Fundamental</option>
+                            <option value="3">3º Fundamental</option>
+                            <option value="4">4º Fundamental</option>
+                            <option value="5">5º Fundamental</option>
+                            <option value="6">6º Fundamental</option>
+                            <option value="7">7º Fundamental</option>
+                            <option value="8">8º Fundamental</option>
+                            <option value="9">9º Fundamental</option>
+                            <option value="10">1º Médio</option>
+                            <option value="11">2º Médio</option>
+                            <option value="12">3º Médio</option>   
+                        </select>
+                    </div>
+                </div>
             </div>
 
             <div class="form-group ">
@@ -155,68 +175,108 @@
     <!-- ABA RESPONSÁVEL -->
     <div class="tab-pane fade" id="responsavel" role="tabpanel" aria-labelledby="responsavel-tab">
         <div class="form">
+        </br>
         <h2>Responsável 01</h2>
-        <label>Nome</label>
-        <input class="form-control" type="text" name="nomeresp1" maxlength="255" autocomplete="off">
-        <spam id="msgNomeResp1"></spam></br>
-        <label>Sexo</label>
-        <select class="form-control" name="sexoresp1" id="">
-            <option value="M">Masculino</option>
-            <option value="F">Feminino</option>
-        </select>
-        
 
-        <label>Data de Nascimento</label>
-        <input type="date" class="form-control" name="datanascimentoresp1">
-        <spam id="msgDataResp1"></spam></br>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-sm-8">
+                    <label>Nome</label>
+                    <input class="form-control" type="text" name="nomeresp1" maxlength="255" autocomplete="off">
+                    <spam id="msgNomeResp1"></spam>
+                </div>
+                
+                <div class="col-sm-2">
+                    <label>Data de Nascimento</label>
+                    <input type="date" class="form-control" name="datanascimentoresp1">
+                    <spam id="msgDataResp1"></spam>
+                </div>
 
-        <label>RG</label>
-        <input type="text" class="form-control" name="rgresp1" autocomplete="off" onkeyup="mascara(this, Rg);" maxlength="8"> 
-        <spam id="msgRgResp1"></spam></br>
-
-        <label>CPF</label>
-        <input type="text" class="form-control" name="cpfresp1" maxlength="11" autocomplete="off" onkeyup="mascara(this, Cpf);">
-        <spam id="msgCpfResp1"></spam></br>
-        
-        <label>Estado Civil</label>
-        <select class="form-control" name="estadocivilresp1" id="">
-            <option value="1">Solteiro</option>
-            <option value="2">Casado</option>
-            <option value="3">Divorciado</option>
-            <option value="4">Viúvo</option>
-            <option value="5">Separado</option>
-        </select>
-
-
-        <label>Profissão</label>
-        <input type="text" class="form-control" name="profissaoresp1">
-
-        <label>Salário</label>
-        <input type="text" class="form-control" name="salarioresp1" id="salarioresp1" onkeyup="mascara(this, Moeda);">
-        <spam id="msgSalarioResp1"></spam></br>
-
-        <label>Local de Trabalho</label>
-        <input type="text" class="form-control" name="trabalhoresp1">
-
-        <label>Escolaridade</label>
-        <select class="form-control" name="escolaridaderesp1" id="">
-            <option value="1">Ensino Fundamental Incompleto</option>
-            <option value="2">Ensino Fundamental Completo</option>
-            <option value="3">Ensino Médio Incompleto</option>
-            <option value="4">Ensino Médio Completo</option>
-            <option value="5">Ensino Superior Incompleto</option>
-            <option value="6">Ensino Superior Completo</option>
-        </select>
-
-        <div class="form-group ">
-            <label>Telefone</label>
-            <input type="text" class="form-control" name="tel1resp1" id="tel1resp1"  onkeyup="mascara(this, Telefone);" maxlength="11">
-            <spam id="msgTel1Resp1"></spam>
+                <div class="col-sm-2">
+                    <label>Sexo</label>
+                    <select class="form-control" name="sexoresp1" id="">
+                        <option value="M">Masculino</option>
+                        <option value="F">Feminino</option>
+                    </select>
+                </div>
+            </div>
         </div>
-        <div class="form-group ">
-            <label>Telefone 2</label>
-            <input type="text" class="form-control" name="tel2resp1" id="tel2resp1" onkeyup="mascara(this, Telefone);" maxlength="11">
-            <spam id="msgTel2Resp1"></spam>
+
+        <div class="form-group">
+            <div class="row">
+                    <div class="col-sm-4">
+                    <label>RG</label>
+                    <input type="text" class="form-control" name="rgresp1" autocomplete="off" onkeyup="mascara(this, Rg);" maxlength="8"> 
+                    <spam id="msgRgResp1"></spam>
+                </div>
+
+                <div class="col-sm-4">
+                    <label>CPF</label>
+                    <input type="text" class="form-control" name="cpfresp1" maxlength="11" autocomplete="off" onkeyup="mascara(this, Cpf);">
+                    <spam id="msgCpfResp1"></spam>
+                </div>
+
+                <div class="col-sm-4">
+                    <label>Estado Civil</label>
+                    <select class="form-control" name="estadocivilresp1" id="">
+                        <option value="1">Solteiro</option>
+                        <option value="2">Casado</option>
+                        <option value="3">Divorciado</option>
+                        <option value="4">Viúvo</option>
+                        <option value="5">Separado</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <div class="col-sm-8">
+                    <label>Profissão</label>
+                    <input type="text" class="form-control" name="profissaoresp1">
+                </div>
+                <div class="col-sm-4">
+                    <label>Salário</label>
+                    <input type="text" class="form-control" name="salarioresp1" id="salarioresp1" onkeyup="mascara(this, Moeda);">
+                    <spam id="msgSalarioResp1"></spam>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <div class="col-sm-8">
+                    <label>Local de Trabalho</label>
+                    <input type="text" class="form-control" name="trabalhoresp1">
+                </div>
+
+                <div class="col-sm-4">
+                    <label>Escolaridade</label>
+                    <select class="form-control" name="escolaridaderesp1" id="">
+                        <option value="1">Ensino Fundamental Incompleto</option>
+                        <option value="2">Ensino Fundamental Completo</option>
+                        <option value="3">Ensino Médio Incompleto</option>
+                        <option value="4">Ensino Médio Completo</option>
+                        <option value="5">Ensino Superior Incompleto</option>
+                        <option value="6">Ensino Superior Completo</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <div class="col-sm-4">
+                    <label>Telefone</label>
+                    <input type="text" class="form-control" name="tel1resp1" id="tel1resp1"  onkeyup="mascara(this, Telefone);" maxlength="11">
+                    <spam id="msgTel1Resp1"></spam>
+                </div>
+                <div class="col-sm-4">
+                    <label>Telefone 2</label>
+                    <input type="text" class="form-control" name="tel2resp1" id="tel2resp1" onkeyup="mascara(this, Telefone);" maxlength="11">
+                    <spam id="msgTel2Resp1"></spam>
+                </div>
+            </div>
         </div>
 
         <label>Observações</label>

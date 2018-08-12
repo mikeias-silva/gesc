b1173915a331f5f39f0d51437927b176ccda1492
mikeias07 <mikeias07@hotmail.com>
2018-08-12 19:35:21 -0300

    font awesome

--- projeto_GESC/resources/views/layout/principal.blade.php
+++ projeto_GESC/resources/views/layout/principal.blade.php
@@ -41,6 +41,7 @@
      <script src="{{ asset('js/app.js') }}" defer></script>
 
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
    
     <link rel="stylesheet" href="/css/style.css">

5afa2920813a94a4176e450dbf7c538bc8ddf77b
Carlos Alexandre <vitorino.17alexandre@gmail.com>
2018-09-15 23:28:19 -0300

    Revisao telas de dashboard

--- projeto_GESC/app/Http/Controllers/DashboardController.php
+++ projeto_GESC/app/Http/Controllers/DashboardController.php
@@ -13,11 +13,13 @@ class DashboardController extends Controller
         $matriculaIdade=[];
         $aux;
         $count=0;
-        $vagas = DB::select('select * from vagas');
+        $ano= date("Y");
+        $vagas = DB::select("select * from vagas where anovaga='{$ano}'");
+        
 
         foreach($vagas as $v){
             $aux=DB::select("select count(idvaga) as numero from matriculas where statuscadastro='Ativo' 
-                                            and idvaga='{$v->idvaga}'");
+                                            and idvaga='{$v->idvaga}' ");
             array_push($vagaOcupada, $aux[0]->numero);
             
         }
@@ -25,13 +27,13 @@ class DashboardController extends Controller
         for($count=1; $count<18; $count++){
             $aux=DB::select("select count(idade) as numroIdade from (select YEAR(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(pessoa.datanascimento))) AS idade 
             from matriculas, crianca, pessoa where crianca.idcrianca=matriculas.idcrianca 
-            && crianca.idpessoa=pessoa.idpessoa && matriculas.statuscadastro=1) as idade where idade='{$count}';");
+            && crianca.idpessoa=pessoa.idpessoa && matriculas.statuscadastro=1 && EXTRACT(YEAR FROM anomatricula)='{$ano}') as idade where idade='{$count}';");
             array_push($matriculaIdade, $aux[0]->numroIdade);
         }
         $mes= date("m");
         $aniversarioMes = DB::select("select pessoa.nomepessoa, DATE_FORMAT(pessoa.datanascimento, '%d/%m/%Y') as datanascimento, YEAR(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(pessoa.datanascimento))) AS idade, turma.GrupoConvivencia from matriculas, crianca, pessoa , turma
         where crianca.idcrianca=matriculas.idcrianca && crianca.idpessoa=pessoa.idpessoa && matriculas.statuscadastro=1
-        && matriculas.idturma=turma.idturma && EXTRACT(MONTH FROM pessoa.datanascimento)='{$mes}';");
+        && matriculas.idturma=turma.idturma && EXTRACT(MONTH FROM pessoa.datanascimento)='{$mes}' && EXTRACT(YEAR FROM anomatricula)='{$ano}';");
         
 
         return view('dashboard.dashboard')->with('vagas', $vagas)->with('vagaOcupada', $vagaOcupada)->with('matriculaIdade', $matriculaIdade)

361ad16ae63c7388acb1a054348483640fc62c71
Carlos Alexandre <vitorino.17alexandre@gmail.com>
2018-10-21 16:55:11 -0300

    Implmentacao do help

---
+++ projeto_GESC/public/js/dashboard.js
@@ -0,0 +1,10 @@
+addEventListener("keydown", function(event) {
+    if (event.keyCode == 112){
+        event.preventDefault();
+        $("#help").modal("show");  
+    }
+  });
+  /*addEventListener("keyup", function(event) {
+    if (event.keyCode == 112)
+      document.body.style.background = "";
+  });*/

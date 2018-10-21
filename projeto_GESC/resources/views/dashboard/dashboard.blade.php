c12c7cca336686536f4762c91df95244bdc94b1a
Carlos Alexandre <vitorino.17alexandre@gmail.com>
2018-10-20 22:36:15 -0300

    Ajustes ficha e inicio do help

--- projeto_GESC/resources/views/dashboard/dashboard.blade.php
+++ projeto_GESC/resources/views/dashboard/dashboard.blade.php
@@ -95,5 +95,34 @@
     </tbody>
 </table>
 
+<div class="modal fade" id="help" tabindex="-1" role="dialog" aria-labelledby="help" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="help">Ajuda</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+            <div class="col-md-12">
+                Teste
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+  addEventListener("keydown", function(event) {
+    if (event.keyCode == 112){
+        event.preventDefault();
+        $("#help").modal("show");  
+        document.body.style.background = "violet";
+    }
+  });
+  addEventListener("keyup", function(event) {
+    if (event.keyCode == 112)
+      document.body.style.background = "";
+  });
+</script>
 
 @stop
